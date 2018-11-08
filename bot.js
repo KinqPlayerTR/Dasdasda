d158fc7669dc546f0054a161959aa74a83cfcc6f
KinqPlayerTR <42252885+KinqPlayerTR@users.noreply.github.com>
2018-11-08 13:00:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,6 +81,9 @@ client.unload = command => {
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
     msg.reply('Aleyküm Selam,  Hoş Geldin Nasilsin');
+client.on('message', msg => {
+  if (msg.content.toLowerCase() === 'naber') {
+    msg.reply('İyi Senden Naber');
   }
 });
 
