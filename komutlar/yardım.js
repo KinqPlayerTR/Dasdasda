ba1d06d7492d4ca2b3c2f5c3f460ee6a01276210
KinqPlayerTR <42252885+KinqPlayerTR@users.noreply.github.com>
2018-11-08 13:17:42 +0300

    Update yardım.js

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -10,7 +10,7 @@ exports.run = (client, message, params) => {
   .setColor(0x00ffff)
   .addField("**Eğlence ve Kullanıcı Komutları:**", `k+banned = Dene ve Gör! \nk+avatarım = Avatarınınızı Gösterir. \nk+herkesebendençay = Herkese Çay Alırsınız. \nk+koş = Koşarsınız.\nk+çayiç = Çay İçersiniz. \nk+çekiç = İstediğiniz Kişiye Çekiç Atarsınız. \nk+çayaşekerat = Çaya Şeker Atarsınız. \nk+yumruh-at = Yumruk Atarsınız. \nk+yaz = Bota İstediğiniz Şeyi Yazdırırsınız. \nk+sunucuresmi = BOT Sunucunun Resmini Atar. \nk+sunucubilgi = BOT Sunucu Hakkında Bilgi Verir. \nk+kullanıcıbilgim = Sizin Hakkınızda Bilgi Verir. `)
   .addField("**Sunucu Yetkilisi Komutları**", `k+ban = İstediğiniz Kişiyi Sunucudan Banlar. \nk+kick  = İstediğiniz Kişiyi Sunucudan Atar. \nk+unban = İstediğiniz Kişinin Yasağını Açar. \nk+sustur = İstediğiniz Kişiyi Susturur. -MUTED- \nk+oylama = Oylama Açar. \nk+duyuru = Güzel Bir Duyuru Görünümü Sağlar.`)
-  .addField("**Botun Ana Komutları**", "k+yardım = BOT Komutlarını Atar. \nk+bilgi = BOT Kendisi Hakkında Bilgi Verir. \nk+ping = BOT Gecikme Süresini Söyler. \nk+davet = BOT Davet Linkini Atar. \k+istatistik = BOT İstatistiklerini Atar.")
+  .addField("**Botun Ana Komutları**", "k+yardım = BOT Komutlarını Atar. \nk+bilgi = BOT Kendisi Hakkında Bilgi Verir. \nk+ping = BOT Gecikme Süresini Söyler. \nk+davet = BOT Davet Linkini Atar. \nk+istatistik = BOT İstatistiklerini Atar.")
   .addField("**Yapımcı Komutları:**", " **Güvenlik Amacı İle Yapımcı Komutları Gösterilmiyor** ")
   .setFooter('**KraliyetBOT**')
   if (!params[0]) {
